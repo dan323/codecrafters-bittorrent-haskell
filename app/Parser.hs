fb0ff416c43116150ebffd408bae65f97548bc54
Daniel de la Concepción <d.concepcion@ocado.com>
2024-10-13 23:39:41 +0200

    integers

--- app/Parser.hs
+++ app/Parser.hs
@@ -4,31 +4,36 @@ where
 
 import Data.ByteString.Char8 (ByteString, readInt, unpack, pack)
 import qualified Data.ByteString as B
-import Text.Megaparsec (Parsec, many, single, satisfy, count, anySingle, parse)
+import Text.Megaparsec (Parsec, many, single, satisfy, count, anySingle, parse, choice)
 import Text.Megaparsec.Byte (digitChar)
-import Data.Void
+import Data.Void ( Void )
+import Data.Functor ( ($>) )
 import Data.Char (isDigit, ord)
 import GHC.Word (Word8)
 import Control.Monad.Identity (Identity)
 import Data.Aeson (Value(Bool))
 import Data.ByteString.Builder (toLazyByteString)
+import Control.Monad.Combinators (option)
 
 type Decoder = Parsec Void ByteString DecodedValue
 type Parser = Parsec Void ByteString
 
 data DecodedValue = ST ByteString | INT Int
 
-intDecoder :: Parser Int
-intDecoder = read . unpack . B.pack <$> many digitChar
+intParser :: Parser Int
+intParser = read . unpack . B.pack <$> many digitChar
 
 stringDecoder :: Decoder
-stringDecoder = intDecoder >>= (\len -> separatorDecoder *> (ST . B.pack <$> count len anySingle))
+stringDecoder = intParser >>= (\len -> separatorParser *> (ST . B.pack <$> count len anySingle))
 
 valueDecoder :: Decoder
-valueDecoder = stringDecoder
+valueDecoder = choice [stringDecoder, intDecoder]
 
-separatorDecoder :: Parser Word8
-separatorDecoder = single (fromIntegral . ord $ ':')
+separatorParser :: Parser Word8
+separatorParser = single (fromIntegral . ord $ ':'::Word8)
+
+intDecoder :: Decoder
+intDecoder = INT <$> (single (fromIntegral . ord $ 'i') *> (option id (single (fromIntegral . ord $ '-') $> negate) <*> intParser) <* single (fromIntegral . ord $ 'e'))
 
 runDecoder :: ByteString -> DecodedValue
 runDecoder input = case parse valueDecoder "" input of
